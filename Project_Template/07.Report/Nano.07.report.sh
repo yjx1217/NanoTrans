2df4d1b4192ae5250336758507f43545aea9f96a
ldyang <ld_yang@outlook.com>
2023-12-14 11:41:54 +0800

    update args

--- Project_Template/07.Report/Nano.07.report.sh
+++ Project_Template/07.Report/Nano.07.report.sh
@@ -7,10 +7,12 @@ source ./../../env.sh
 
 #######################################
 # set project-specific variables
-wkdir=${PWD}
 batch_id="Batch_Example" # The batch_id used for the processing batch. Default = "Batch_Example".
 master_sample_table="Master_Sample_Table.$batch_id.txt" # The master sample table for the processing batch. Default = "Master_Sample_Table.Batch_polyA.txt".
 ############################################################
+# Normally no need to change the following settings.
+wkdir=${PWD}
+##########################################################
 
 source $miniconda3_dir/activate  $build_dir/quarto_conda_env
 quarto render NanoTrans_Report.qmd -P "wkdir:${wkdir%/*}" -P "dataset:${batch_id}"
