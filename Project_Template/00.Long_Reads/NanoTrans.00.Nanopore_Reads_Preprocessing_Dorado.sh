20779ee7df87b6a888bb3e98505802032a7b64a8
ldyang <ld_yang@outlook.com>
2024-04-10 20:14:49 +0800

    fix dorado bash script

--- Project_Template/00.Long_Reads/NanoTrans.00.Nanopore_Reads_Preprocessing_Dorado.sh
+++ Project_Template/00.Long_Reads/NanoTrans.00.Nanopore_Reads_Preprocessing_Dorado.sh
@@ -5,7 +5,7 @@ set -e -o pipefail
 source ./../../env.sh
 
 ########################
-dorado_run_device="cpu" # The running device of basecalling: "cuda:0,...,N", "cuda:all", "cpu". Default = "cpu".
+dorado_run_device="cpu" # The running device of basecalling: "cuda:0,...,N", "cuda:all", or "cpu". Default = "cpu".
 if [[ $dorado_run_device != "cpu" ]]
 then
     # set CUDA environment for GPU
@@ -21,23 +21,22 @@ export CPLUS_INCLUDE_PATH=$C_INCLUDE_PATH
 
 #######################################
 # set project-specific variables
-
 sample_id="vir1.rep1" # The flowcell ID of the nanopore run. Default = "vir1.rep1".
-raw_fast5_dir="./raw_fast5/$sample_id" # The directory containing the raw nanopore reads before basecalling. Default = "./raw_fast5/$sample_id".
-decode_models_name="rna002_70bps_hac@v3" # The RNA model name used to basecalling. For more details, please to see: https://github.com/nanoporetech/dorado?tab=readme-ov-file#rna-models. Default = "rna002_70bps_hac@v3".
+raw_fast5_dir="./raw_fast5/$sample_id" # The directory containing the raw nanopore reads, fast5 or pod5 format, before basecalling. Default = "./raw_fast5/$sample_id".
+decode_models_name="rna002_70bps_hac@v3" # The RNA model name used for basecalling. For more details, please to see: https://github.com/nanoporetech/dorado?tab=readme-ov-file#rna-models. Default = "rna002_70bps_hac@v3".
 
 #############################
 # Normally no need to change the following parameter settings.
-run_basecalling="yes" # Whether to perform basecalling: "yes" or "no". Default = "yes". 
-run_nanoplotting="yes" # Whether to perform nanoplotting: "yes" or "no". Default = "yes". 
+threads=8 # The number of CPU threads to use. Default = 8.
+run_basecalling="yes"   # Whether to perform basecalling:    "yes" or "no". Default = "yes". 
+run_nanoplotting="yes"  # Whether to perform nanoplotting:   "yes" or "no". Default = "yes". 
 run_demultiplexing="no" # Whether to perform demultiplexing: "yes" or "no". Default = "no". 
 barcode_kit_version="" # The barcode kit version of the nanopore run. Default = "".
 basecalled_fastq_dir="./basecalled_fastq/$sample_id" # The directory containing the basecalled fastq reads. This directory will be automatically generated when running basecalling.
 basecalled_summary_dir="./basecalled_summary/$sample_id" # The directory containing nanoplot summary outputs. This directory will be automatically generated when running nanoplot.
 qual=5 # read quality filter for basecalling. Default = 5.
-demultiplexing_threads=8 # The number of threads to use for demultiplexing. Default = "8".
+demultiplexing_threads=$threads # The number of threads to use for demultiplexing. Default = 8.
 demultiplexed_fastq_dir="$basecalled_fastq_dir/demultiplexed_fastq" # The directory containing the demultiplexed basecalled nanopore reads. This directory will be automatically generated when running demultiplexing. 
-
 ##############################
 
 wkdir=$(pwd)
@@ -50,38 +49,31 @@ fi
 
 if [[ "$run_basecalling" == "yes" ]]
 then
-    echo "Check if $basecalled_fast5_dir is empty for running basecalling."
-    if [[ -d $basecalled_fast5_dir && "$(ls $basecalled_fast5_dir)" ]]
-    then
-    echo "Warning! The basecalled fast5 directory $basecalled_fast5_dir exists and it is not empty! Please empty its content if you want to run basecalling."
-    echo "Exit!!!"
-    exit
-    elif [[ -d $basecalled_fastq_dir && "$(ls $basecalled_fastq_dir)" ]]
+    echo "Check if $basecalled_fastq_dir is empty for running basecalling."
+
+    if [[ -d $basecalled_fastq_dir && "$(ls $basecalled_fastq_dir)" ]]
     then
         echo "Warning! The basecalled fastq directory $basecalled_fastq_dir exists and it is not empty! Please empty its content if you want to run basecalling."
         echo "Exit!!!"
         exit
     else
-    echo "Check passed!"
-    echo "Running basecalling .."
-    if [[ ! -d $basecalled_fast5_dir ]]
-    then
-        mkdir -p $basecalled_fast5_dir
-    fi
-    if [[ ! -d $basecalled_fastq_dir ]]
-    then
-        mkdir -p $basecalled_fastq_dir
-    fi
+        echo "Check passed!"
+        echo "Running basecalling .."
+        if [[ ! -d $basecalled_fastq_dir ]]
+        then
+            mkdir -p $basecalled_fastq_dir
+        fi
 
-    $dorado download --model ${decode_models_name}
-    $dorado basecaller \
-        --device $dorado_run_device \
-        --min-qscore $qual \
-        --emit-fastq \
-        --recursive \
-        --verbose \
-        ${decode_models_name} \
-        $raw_fast5_dir | gzip -c > ${basecalled_fastq_dir}/${sample_id}.basecalled_reads.Q${qual}.pass.fastq.gz
+        $dorado_dir/dorado download --model ${decode_models_name}
+        $dorado_dir/dorado basecaller \
+            --device $dorado_run_device \
+            --min-qscore $qual \
+            --emit-fastq \
+            --recursive \
+            --verbose \
+            ${decode_models_name} \
+            $raw_fast5_dir | gzip -c > ${basecalled_fastq_dir}/${sample_id}.basecalled_reads.Q${qual}.pass.fastq.gz
+    fi
 fi
 
 cd $wkdir
@@ -91,7 +83,7 @@ then
     if [[ "$(ls $basecalled_fastq_dir)" ]]
     then
         echo "Running demultiplexing."
-        $dorado demux \
+        $dorado_dir/dorado demux \
             --kit-name $barcode_kit_version \
             --emit-fastq \
             --output-dir $demultiplexed_fastq_dir \
@@ -122,36 +114,36 @@ then
     echo "Check if $basecalled_fastq_dir has basecalled reads for running nanoplotting."
     if [[ "$(ls $basecalled_fastq_dir)" ]]
     then
-    echo "Running nanoplotting."
-    mkdir -p $basecalled_summary_dir
-    fastq_input="$basecalled_fastq_dir/$sample_id.basecalled_reads.Q${qual}.pass.fastq.gz"
-    $nanoplot_dir/NanoPlot \
-        --threads $threads \
-        --fastq $fastq_input \
-        --N50 \
-        -o "$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_NanoPlot_out"
-    fi
-    if [[ "$run_demultiplexing" == "yes" ]]
-    then
-    cd $demultiplexed_fastq_dir
-    for b in ${barcode_kit_version}_barcode*.fastq
-    do
-        barcode_id=${b%.*}
-        echo "for nanoplotting: barcode=$barcode_id"
-        fastq_input="./$sample_id.basecalled_reads.Q${qual}.pass.${b}.gz"
+        echo "Running nanoplotting."
+        mkdir -p $basecalled_summary_dir
+        fastq_input="$basecalled_fastq_dir/$sample_id.basecalled_reads.Q${qual}.pass.fastq.gz"
         $nanoplot_dir/NanoPlot \
-        --threads $threads \
-        --fastq $fastq_input \
-        --N50 \
-        -o "./../../../$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_${barcode_id}_NanoPlot_out"
-    done
-    echo "for nanoplotting: unclassified"
-    fastq_input="./$sample_id.basecalled_reads.Q${qual}.pass.unclassified.fastq.gz"
-    $nanoplot_dir/NanoPlot \
             --threads $threads \
             --fastq $fastq_input \
             --N50 \
-            -o "./../../../$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_unclassified_NanoPlot_out" 
+            -o "$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_NanoPlot_out"
+    fi
+    if [[ "$run_demultiplexing" == "yes" ]]
+    then
+        cd $demultiplexed_fastq_dir
+        for b in ${barcode_kit_version}_barcode*.fastq
+        do
+            barcode_id=${b%.*}
+            echo "for nanoplotting: barcode=$barcode_id"
+            fastq_input="./$sample_id.basecalled_reads.Q${qual}.pass.${b}.gz"
+            $nanoplot_dir/NanoPlot \
+            --threads $threads \
+            --fastq $fastq_input \
+            --N50 \
+            -o "./../../../$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_${barcode_id}_NanoPlot_out"
+        done
+        echo "for nanoplotting: unclassified"
+        fastq_input="./$sample_id.basecalled_reads.Q${qual}.pass.unclassified.fastq.gz"
+        $nanoplot_dir/NanoPlot \
+                --threads $threads \
+                --fastq $fastq_input \
+                --N50 \
+                -o "./../../../$basecalled_summary_dir/${sample_id}_basecalled_reads_Q${qual}_pass_unclassified_NanoPlot_out" 
     fi
 fi
 
